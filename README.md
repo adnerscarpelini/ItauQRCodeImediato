ff689724941a2c4dcd556260c7b37e87b8cf9d5c
Adner Scarpelini <adner.scarpelini@outlook.com>
2024-08-03 14:35:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ItauQRCodeImediato
+ Exemplo de uso da API  de QR Code imediato do Banco Itaú
---
+++ WinClient/FrmInicio.cs
@@ -0,0 +1,10 @@
+namespace WinClient
+{
+    public partial class FrmInicio : Form
+    {
+        public FrmInicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
